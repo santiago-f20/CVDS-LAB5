0d4e7d6e6393a4d2291c878d38164f96419979f5
unknown <santiago.fetecua@labinfo.is.escuelaing.edu.co>
2022-03-05 08:08:31 -0500

    Inicio Lab

---
+++ Servlet/src/main/java/edu/eci/cvds/servlet/SampleServlet.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.eci.cvds.servlet;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Optional;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    urlPatterns = "/helloServlet"
+)
+public class SampleServlet extends HttpServlet{
+    static final long serialVersionUID = 35L;
+
+    @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       Writer responseWriter = resp.getWriter();
+       Optional<String> optName = Optional.ofNullable(req.getParameter("name"));
+       String name = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
+
+       resp.setStatus(HttpServletResponse.SC_OK);
+       responseWriter.write("Hello" + name + "!");
+       responseWriter.flush();
+   }
+}
